5098952f3c6f4a2d8547fdfddce6fb2ee161be8c
BIBINROYBJ <bjbibin.royip@gmail.com>
2024-03-22 21:31:52 +0530

    feat: completed feature

--- ToDo/ToDo/urls.py
+++ ToDo/ToDo/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from tasks import views
 
 urlpatterns = [
+
+    path('',views.index, name="tasks"),
+
+    path('del/<str:item_id>',views.remove, name="del"),
     path('admin/', admin.site.urls),
+    path('',include('tasks.urls'))
 ]
